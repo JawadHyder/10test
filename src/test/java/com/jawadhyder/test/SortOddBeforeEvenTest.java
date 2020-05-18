92b44c872fbce6b6d80dcc2ce815ae1cfa883d17
Jawad Hyder <jawadhyder@roshnirides.com>
2020-05-18 11:39:26 +0500

    more cases

--- src/test/java/com/jawadhyder/test/SortOddBeforeEvenTest.java
+++ src/test/java/com/jawadhyder/test/SortOddBeforeEvenTest.java
@@ -10,5 +10,20 @@ public class SortOddBeforeEvenTest {
         int[] sorted = SortOddBeforeEven.sort(a);
         Assert.assertArrayEquals(sorted, new int[]{1, 3, 7, 2, 4, 16});
 
+        // Last even
+        int[] b = new int[]{1, 3, 5, 7, 16};
+        int[] sorted2 = SortOddBeforeEven.sort(b);
+        Assert.assertArrayEquals(sorted2, new int[]{1, 3, 5, 7, 16});
+
+        // All odd
+        int[] c = new int[]{1, 3, 5, 7};
+        int[] sorted3 = SortOddBeforeEven.sort(c);
+        Assert.assertArrayEquals(sorted3, new int[]{1, 3, 5, 7});
+
+
+        // All even
+        int[] d = new int[]{2, 4, 8, 16};
+        int[] sorted4 = SortOddBeforeEven.sort(d);
+        Assert.assertArrayEquals(sorted4, new int[]{2, 4, 8, 16});
     }
 }
